b20fc245f557aa9af1ca518bc5136758a2b3f468
markDeLaFleur <Mark.DeLaFleur@gmail.com>
2023-02-09 15:30:47 -0500

    adding routing based on which Icon is clicked.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,9 @@
 <script>
-
-import Footer from './Footer.svelte';
 import Table from './Table.svelte';	
-
 </script>
 <body>
-  <h1 class="text-base text-blue-400 underline">Mexican Train Domino Counter and Score Keeper</h1>
+  <h1 class="text-base text-blue-400 underline">
+    Mexican Train Domino Counter and Score Keeper</h1>
 <p> <br>  </p>
-<div class="flex flex-col h-screen">
-  <div class="flex-grow overflow-auto">
   <Table  />
- 
-  </div>
-</div>
-  
-  
 </body>  
