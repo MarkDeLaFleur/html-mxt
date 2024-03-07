0ed7ae7f21e4498c116755ec111bdba00eb27d76
markDeLaFleur <Mark.DeLaFleur@gmail.com>
2024-03-07 15:17:07 -0500

    should auto deploy to github pages

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,2 +1,3 @@
+export const prerender = true;
 export const ssr = false;  //this is necessary to use the localStorage to save data and it 
 // has to go in +layour.js not .svelte. otherwise you'll get these nasty errors about localStorage not found
