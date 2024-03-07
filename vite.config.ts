5005cd34c9f7b65e0d6e5d426207c785f4a3c5b9
markDeLaFleur <Mark.DeLaFleur@gmail.com>
2024-03-07 11:54:40 -0500

    i guess

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
+import { defineConfig } from 'vite';
 
-import {defineConfig} from'vite'
-
-
-const config: UserConfig = {
-	plugins: [sveltekit(),mkcert()],
-	server: {https:true}
-
-	
-};
 export default defineConfig({
-	server: {
-		https: true,
-		host: true
-	  },
-	  
- 
-  plugins: [mkcert(),sveltekit()]
+	server:{https:true,
+	host: true},
+	plugins: [sveltekit(),mkcert()]
 });
-
