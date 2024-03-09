8344306f419371598d53575d1255b4c55d2acaad
markDeLaFleur <Mark.DeLaFleur@gmail.com>
2024-03-09 15:02:40 -0500

    adjustments

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,3 +1,3 @@
-export const prerender = true;
+export const prerender = auto;
 export const ssr = false;  //this is necessary to use the localStorage to save data and it 
-// has to go in +layour.js not .svelte. otherwise you'll get these nasty errors about localStorage not found
+// has to go in +layout.js not .svelte. otherwise you'll get these nasty errors about localStorage not found
