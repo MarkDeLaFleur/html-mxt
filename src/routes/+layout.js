b5147af8e89635a17b6bf6bcf2396fac2221802c
markDeLaFleur <Mark.DeLaFleur@gmail.com>
2024-03-10 14:03:17 -0400

    prerender = false  and if this errors then I guess

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,3 +1,3 @@
-export const prerender = 'auto';
+export const prerender = false;  //fuck it. if this doesn't work.
 export const ssr = false;  //this is necessary to use the localStorage to save data and it 
 // has to go in +layout.js not .svelte. otherwise you'll get these nasty errors about localStorage not found
