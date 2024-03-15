c59e45ec9650bb8376a26db8a96c0606fbc8cec0
markDeLaFleur <Mark.DeLaFleur@gmail.com>
2024-03-15 13:15:06 -0400

    fix error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@ onMount(  () => {
   //check and see if any of the player names in the player score table are initialized
   let checkIt = false;
     $playerScore.forEach(element => {if(element.playerName.length > 0) checkIt= true})
-    if (checkIt != true) {goto("${base}/newGame")};
+    if (checkIt != true) {goto({base}+'/newGame')};
     }
     );
 </script>
